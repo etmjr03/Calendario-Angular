ae2af3d1b824b583ae4c8810a6e46ed73b640396
etmjr03 <etmjr03@gmail.com>
2022-12-21 16:16:36 -0300

    Adicionando funcoes de listagem em modal, rotas e delete

--- src/app/features/schedules/schedules-list/schedules-list.component.ts
+++ src/app/features/schedules/schedules-list/schedules-list.component.ts
@@ -1,9 +1,11 @@
+import { Router } from '@angular/router';
 import { ScheduleService } from './../schedule.service';
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { CalendarView, CalendarEvent } from 'angular-calendar';
+import { CalendarView, CalendarEvent, CalendarEventAction } from 'angular-calendar';
 import { MonthViewDay } from 'calendar-utils';
 import { isSameDay, isSameMonth, parse, parseISO } from 'date-fns';
 import { Schedule } from '../schedule';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-schedules-list',
@@ -23,6 +25,8 @@ export class SchedulesListComponent implements OnInit {
   modalData!: { schedule: Schedule };
 
   constructor(
+    private router: Router,
+    private modal: NgbModal,
     private ScheduleService: ScheduleService
   ) { }
 
@@ -50,17 +54,52 @@ export class SchedulesListComponent implements OnInit {
     }
   }
 
+  onEventClick(event: CalendarEvent) {
+    this.modalData = {
+      schedule: event.meta
+    }
+    
+    this.modal.open(this.modalContent, {
+      size: 'md',
+    })
+  }
+
+  private buildEventActions(schedule: Schedule) {
+    const events: CalendarEventAction[] = [];
+
+    events.push({
+      label: '<i class="fa-solid fa-pencil text-purple mx-1"></i>',
+      onClick: (): void => {
+        this.router.navigate(['schedules', schedule.id])
+      }
+    });
+
+    events.push({
+      label: '<i class="fa-solid fa-trash text-purple mx-1"></i>',
+      onClick: (): void => {
+        this.ScheduleService.delete(schedule.id).subscribe(() => {
+          this.loadSchedules();
+          this.closeActiveDay();
+        });
+      }
+    });
+
+    return events;
+  }
+
   private buildEvents(schedule: Schedule) {
     const parsedDate = parseISO(schedule.date)
     const event: CalendarEvent = {
       title: schedule.title,
       start: parse(schedule.initTime, 'HH:mm', parsedDate),
       end: parse(schedule.endTime, 'HH:mm', parsedDate),
-      cssClass: 'event.-body',
+      actions: this.buildEventActions(schedule),
+      cssClass: 'event-body',
       color: {
         primary: 'var(--purple)',
         secondary: 'var(--bg-prple-alpha)'
-      }
+      },
+      meta: schedule
     }
     return event;
   }
